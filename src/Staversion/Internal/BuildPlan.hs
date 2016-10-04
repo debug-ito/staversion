8a497b68c6bd992da316c2c83f09458a2d886b8f
Toshio Ito <debug.ito@gmail.com>
2016-10-04 22:40:33 +0900

    implementing BuildPlan

--- src/Staversion/Internal/BuildPlan.hs
+++ src/Staversion/Internal/BuildPlan.hs
@@ -10,16 +10,28 @@ module Staversion.Internal.BuildPlan
          packageVersion
        ) where
 
-import Distribution.Package (PackageName)
+import Control.Applicative (empty)
+import Data.Aeson (FromJSON(..), (.:), Value(..), Object)
+import qualified Data.HashMap.Strict as HM
+import Data.Text (Text, unpack)
 import Distribution.Version (Version)
 
 -- | A data structure that keeps a map between package names and their
 -- versions.
-data BuildPlan
+newtype BuildPlan = BuildPlan (HM.HashMap Text Version)
+
+instance FromJSON BuildPlan where
+  parseJSON (Object o) = toBuildPlan =<< (o .: "packages") where
+    toBuildPlan (Object o) = BuildPlan <$> traverse parsePackageObject o
+    toBuildPlan _ = empty
+    parsePackageObject (Object o) = parseVersionText =<< (o .: "version")
+    parsePackageObject _ = empty
+    parseVersionText = undefined -- TODO: Text -> Parser Version
+  parseJSON _ = empty
 
 -- | Load a 'BuildPlan' from a file.
 loadBuildPlanYAML :: FilePath -> IO BuildPlan
 loadBuildPlanYAML = undefined
 
-packageVersion :: BuildPlan -> PackageName -> Maybe Version
+packageVersion :: BuildPlan -> Text -> Maybe Version
 packageVersion = undefined
--- test/Staversion/Internal/BuildPlanSpec.hs
+++ test/Staversion/Internal/BuildPlanSpec.hs
@@ -1,6 +1,5 @@
 module Staversion.Internal.BuildPlanSpec (main,spec) where
 
-import Distribution.Package (PackageName(..))
 import Distribution.Version (Version(..))
 import Test.Hspec
 
@@ -14,5 +13,5 @@ spec = describe "packageVersion" $ do
   describe "lts-4.2" $ do
     specify "conduit -> 1.2.6.1" $ do
       plan <- loadBuildPlanYAML "test/data/lts-4.2.yaml"
-      packageVersion plan (PackageName "conduit") `shouldBe` Just (Version [1,2,6,1] [])
+      packageVersion plan "conduit" `shouldBe` Just (Version [1,2,6,1] [])
 
