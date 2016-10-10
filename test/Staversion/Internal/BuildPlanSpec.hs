d6e8417f9ff986f7f79ecf13604e4948568eee03
Toshio Ito <debug.ito@gmail.com>
2016-10-10 20:58:20 +0900

    BuildPlanSpec: add tests for querying core packages. failing.

--- test/Staversion/Internal/BuildPlanSpec.hs
+++ test/Staversion/Internal/BuildPlanSpec.hs
@@ -36,10 +36,16 @@ packageVersion_spec = describe "packageVersion" $ do
       loadVersion "drawille" loader `shouldReturn` Just (Version [0,1,0,6] [])
     specify "unknown -> Nothing" $ do
       loadVersion "unknown" loader `shouldReturn` Nothing
+    specify "ghc -> 7.10.3" $ do
+      loadVersion "ghc" loader `shouldReturn` Just (Version [7,10,3] [])
+    specify "base -> 4.8.2.0" $ do
+      loadVersion "base" loader `shouldReturn` Just (Version [4,8,2,0] [])
 
   forBuildPlan "lts-4.2" $ \loader -> do
     specify "conduit -> 1.2.6.1" $ do
       loadVersion "conduit" loader `shouldReturn` Just (Version [1,2,6,1] [])
+    specify "transformers -> 0.4.2.0" $ do
+      loadVersion "transformers" loader `shouldReturn` Just (Version [0,4,2,0] [])
 
 
 forBuildPlan :: String -> (IO BuildPlan -> Spec) -> Spec
