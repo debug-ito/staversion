24598fe92b53e96ffd216cddb7a31d4fb752dc60
Toshio Ito <debug.ito@gmail.com>
2016-10-23 17:27:15 +0900

    Stackage: implement parseResolverString. failing.

--- src/Staversion/Internal/BuildPlan/Stackage.hs
+++ src/Staversion/Internal/BuildPlan/Stackage.hs
@@ -13,7 +13,10 @@ module Staversion.Internal.BuildPlan.Stackage
          fetchBuildPlanYAML
        ) where
 
+import Control.Monad (void)
+import Control.Applicative ((<|>), (*>))
 import qualified Data.Text.Lazy as TL
+import qualified Text.ParserCombinators.ReadP as P
 
 import Staversion.Internal.Query (Resolver)
 
@@ -30,7 +33,28 @@ data PartialResolver = PartialExact ExactResolver
                      deriving (Show,Eq,Ord)
 
 parseResolverString :: Resolver -> Maybe PartialResolver
-parseResolverString = undefined
+parseResolverString = getResult . P.readP_to_S parser where
+  getResult [] = Nothing
+  getResult ((ret, _) : _) = Just ret
+  p_read = P.readS_to_P reads
+  parser = lts <|> nightly
+  lts = P.string "lts" *> ( lts_exact <|> lts_major <|> pure PartialLTSLatest )
+  lts_exact = do
+    void $ P.char '-'
+    major <- p_read
+    void $ P.char '.'
+    minor <- p_read
+    return $ PartialExact $ ExactLTS major minor
+  lts_major = P.char '-' *> ( PartialLTSMajor <$> p_read )
+  nightly = P.string "nightly" *> ( nightly_exact <|> pure PartialNightlyLatest )
+  nightly_exact = do
+    void $ P.char '-'
+    year <- p_read
+    void $ P.char '-'
+    month <- p_read
+    void $ P.char '-'
+    day <- p_read
+    return $ PartialExact $ ExactNightly year month day
 
 formatResolverString :: PartialResolver -> Resolver
 formatResolverString = undefined
