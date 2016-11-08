4deaaea59e81abc2ce228c2a48fa7aa542f23fe8
Toshio Ito <debug.ito@gmail.com>
2016-11-08 21:58:09 +0900

    Query: revise sourceDesc for Hackage.

--- src/Staversion/Internal/Query.hs
+++ src/Staversion/Internal/Query.hs
@@ -57,4 +57,4 @@ resultVersionsToList (ResultVersions m) = HM.toList m
 -- | description of a 'PackageSource'.
 sourceDesc :: PackageSource -> Text
 sourceDesc (SourceStackage r) = pack r
-sourceDesc SourceHackage = "hackage"
+sourceDesc SourceHackage = "latest in hackage"
