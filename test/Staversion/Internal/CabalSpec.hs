9418acf9043138f774153024716d86368d2f2277
Toshio Ito <debug.ito@gmail.com>
2016-12-23 11:25:40 +0900

    CabalSpec: add a new spec "conduit.cabal". failing.

--- test/Staversion/Internal/CabalSpec.hs
+++ test/Staversion/Internal/CabalSpec.hs
@@ -8,50 +8,105 @@ import Staversion.Internal.Cabal (loadCabalFile, BuildDepends(..), Target(..))
 main :: IO ()
 main = hspec spec
 
+shouldBeParsedTo :: FilePath -> [BuildDepends] -> IO ()
+shouldBeParsedTo cabal_file expectation = do
+  got_deps <- either error return  =<< loadCabalFile ("test" </> "data" </> cabal_file)
+  got_deps `shouldBe` expectation
+
 spec :: Spec
 spec = describe "loadCabalFile" $ do
   it "should load library, executable and test-suite targets" $ do
-    got_deps <- either error return  =<< loadCabalFile ("test" </> "data" </> "doctest.cabal")
-    got_deps `shouldBe` [ BuildDepends { depsTarget = TargetLibrary,
-                                         depsPackages = [ "base",
-                                                          "base-compat",
-                                                          "ghc",
-                                                          "syb",
-                                                          "deepseq",
-                                                          "directory",
-                                                          "filepath",
-                                                          "process",
-                                                          "ghc-paths",
-                                                          "transformers"
-                                                        ]
-                                       },
-                          BuildDepends { depsTarget = TargetExecutable "doctest",
-                                         depsPackages = [ "base",
-                                                          "doctest"
-                                                        ]
-                                       },
-                          BuildDepends { depsTarget = TargetTestSuite "spec",
-                                         depsPackages = [ "base",
-                                                          "ghc",
-                                                          "syb",
-                                                          "deepseq",
-                                                          "directory",
-                                                          "filepath",
-                                                          "process",
-                                                          "ghc-paths",
-                                                          "transformers",
-                                                          "base-compat",
-                                                          "HUnit",
-                                                          "hspec",
-                                                          "QuickCheck",
-                                                          "stringbuilder",
-                                                          "silently",
-                                                          "setenv",
-                                                          "with-location"
-                                                        ]
-                                       },
-                          BuildDepends { depsTarget = TargetTestSuite "doctests",
-                                         depsPackages = [ "base", "doctest"
-                                                        ]
-                                       }
-                        ]
+    "doctest.cabal" `shouldBeParsedTo`
+      [ BuildDepends { depsTarget = TargetLibrary,
+                       depsPackages = [ "base",
+                                        "base-compat",
+                                        "ghc",
+                                        "syb",
+                                        "deepseq",
+                                        "directory",
+                                        "filepath",
+                                        "process",
+                                        "ghc-paths",
+                                        "transformers"
+                                      ]
+                     },
+        BuildDepends { depsTarget = TargetExecutable "doctest",
+                       depsPackages = [ "base",
+                                        "doctest"
+                                      ]
+                     },
+        BuildDepends { depsTarget = TargetTestSuite "spec",
+                       depsPackages = [ "base",
+                                        "ghc",
+                                        "syb",
+                                        "deepseq",
+                                        "directory",
+                                        "filepath",
+                                        "process",
+                                        "ghc-paths",
+                                        "transformers",
+                                        "base-compat",
+                                        "HUnit",
+                                        "hspec",
+                                        "QuickCheck",
+                                        "stringbuilder",
+                                        "silently",
+                                        "setenv",
+                                        "with-location"
+                                      ]
+                     },
+        BuildDepends { depsTarget = TargetTestSuite "doctests",
+                       depsPackages = [ "base", "doctest"
+                                      ]
+                     }
+      ]
+  it "should merge conditional build-depends" $ do
+    "conduit.cabal" `shouldBeParsedTo`
+      [ BuildDepends { depsTarget = TargetLibrary,
+                       depsPackages = [ "base",
+                                        "resourcet",
+                                        "exceptions",
+                                        "lifted-base",
+                                        "transformers-base",
+                                        "transformers",
+                                        "mtl",
+                                        "mmorph",
+                                        "monad-control",
+                                        "void"
+                                      ]
+                     },
+        BuildDepends { depsTarget = TargetTestSuite "test",
+                       depsPackages = [ "conduit",
+                                        "base",
+                                        "hspec",
+                                        "QuickCheck",
+                                        "transformers",
+                                        "mtl",
+                                        "resourcet",
+                                        "containers",
+                                        "exceptions",
+                                        "safe",
+                                        "void"
+                                      ]
+                     },
+        BuildDepends { depsTarget = TargetBenchmark "optimize-201408",
+                       depsPackages = [ "base",
+                                        "conduit",
+                                        "vector",
+                                        "deepseq",
+                                        "containers",
+                                        "transformers",
+                                        "hspec",
+                                        "mwc-random",
+                                        "criterion",
+                                        "kan-extensions"
+                                      ]
+                     },
+        BuildDepends { depsTarget = TargetBenchmark "unfused",
+                       depsPackages = [ "base",
+                                        "conduit",
+                                        "criterion",
+                                        "transformers"
+                                      ]
+                     }
+      ]
