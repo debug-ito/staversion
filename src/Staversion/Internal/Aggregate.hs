ee749338d38905b892db0d5fc3a60efa4e0ec1be
Toshio Ito <debug.ito@gmail.com>
2017-02-19 12:01:43 +0900

    Aggregate: add signature of 'aggregateResults' function.

--- src/Staversion/Internal/Aggregate.hs
+++ src/Staversion/Internal/Aggregate.hs
@@ -7,6 +7,7 @@
 module Staversion.Internal.Aggregate
        ( Aggregator,
          VersionRange,
+         aggregateResults,
          showVersionRange,
          aggOr
        ) where
@@ -19,6 +20,9 @@ import qualified Distribution.Version as V
 import qualified Distribution.Text as DT
 import qualified Text.PrettyPrint as Pretty
 
+import Staversion.Internal.Log (LogEntry)
+import Staversion.Internal.Result (Result, AggregatedResult)
+
 -- | Aggregate some 'Version's into a 'VersionRange'.
 type Aggregator = NonEmpty Version -> VersionRange
 
@@ -30,4 +34,13 @@ aggOr :: Aggregator
 aggOr vs = foldr f (V.thisVersion $ NL.last svs) $ NL.init svs where
   svs = NL.nub $ NL.sort vs
   f elem_v range = V.unionVersionRanges (V.thisVersion elem_v) range
+
+-- | Aggregate 'Result's with the given 'Aggregator'. It first groups
+-- 'Result's based on its 'resultFor' field, and then each group is
+-- aggregated into an 'AggregatedResult'.
+--
+-- If it fails, it returns an empty list of 'AggregatedResult'. It
+-- also returns a list of 'LogEntry's to report warnings and errors.
+aggregateResults :: Aggregator -> [Result] -> ([AggregatedResult], [LogEntry])
+aggregateResults = undefined
 
