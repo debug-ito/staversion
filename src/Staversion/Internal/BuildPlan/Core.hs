7c0f6837c3ee73aa08f278e49ce74004c30168ab
Toshio Ito <debug.ito@gmail.com>
2020-10-02 14:21:43 +0900

    Core: implement parseGHCPkgVersions

--- src/Staversion/Internal/BuildPlan/Core.hs
+++ src/Staversion/Internal/BuildPlan/Core.hs
@@ -21,17 +21,26 @@ module Staversion.Internal.BuildPlan.Core
     fetchGHCPkgVersions
   ) where
 
+import Control.Monad (void)
+import Control.Applicative ((<|>), (<$>), some, optional)
 import qualified Data.ByteString.Lazy as BSL
+import Data.Char (isSpace, isDigit)
 import Data.Foldable (foldlM)
 import Data.Hashable (Hashable(..))
 import qualified Data.HashMap.Strict as HM
+import Data.Semigroup ((<>))
 import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TL
 import GHC.Generics (Generic)
 
 import Staversion.Internal.HTTP (Manager)
 import Staversion.Internal.Query (PackageName)
-import Staversion.Internal.Version (Version, versionNumbers, mkVersion)
+import Staversion.Internal.Version (Version, versionNumbers, mkVersion, parseVersionText)
 import Staversion.Internal.BuildPlan.BuildPlanMap (BuildPlanMap,  HasVersions(..))
+import qualified Staversion.Internal.BuildPlan.BuildPlanMap as BuildPlanMap
+import qualified Staversion.Internal.Megaparsec as P
 
 -- | Name of a compiler
 type CompilerName = Text
@@ -78,20 +87,47 @@ ghcName = "ghc"
 type PkgVersions = HM.HashMap Compiler CoreBuildPlanMap
 
 addVersions :: Compiler -> [(PackageName, Version)] -> PkgVersions -> PkgVersions
-addVersions = undefined -- TODO
+addVersions c pkgs = HM.insertWith merge c inserted_cbp
+  where
+    inserted_cbp = CoreBuildPlanMap c $ BuildPlanMap.fromList pkgs
+    merge new old = new { coreMap = coreMap new <> coreMap old }
 
 parsePkgVersionsLine :: Text -> Either String (Compiler, [(PackageName, Version)])
-parsePkgVersionsLine = undefined -- TODO
+parsePkgVersionsLine input = mapError $ P.runParser parser "" input
+  where
+    mapError (Left e) = Left $ show e
+    mapError (Right a) = Right a
+    parser = do
+      P.space
+      cv <- parserCompilerVersion
+      P.space1
+      vers <- P.sepBy parserPVer P.space1
+      return (Compiler ghcName cv, vers)
+    parserCompilerVersion = headVersion <|> (CVNumbered <$> parserVersion)
+    headVersion = do
+      void $ P.string "HEAD"
+      return CVHead
+    parserVersion = do
+      vstr <- P.textSatisfying (\c -> isDigit c || c == '.')
+      void $ optional $ P.char '*'
+      case parseVersionText vstr of
+        Nothing -> fail ("Cannot parse to a version: " ++ T.unpack vstr)
+        Just v -> return v
+    parserPVer = do
+      name <- P.textSatisfying (\c -> c /= '/')
+      void $ P.char '/'
+      ver <- parserVersion
+      return (name, ver)
 
 -- | Parse the \"pkg_versions.txt\" file for GHC core packages.
 parseGHCPkgVersions :: BSL.ByteString -> Either String (HM.HashMap Compiler CoreBuildPlanMap)
 parseGHCPkgVersions content =
-  foldlM f HM.empty $ filter isWhiteLine $ map removeComment $ toLines content
+  foldlM f HM.empty $ filter (not . isWhiteLine) $ map removeComment $ toLines content
   where
     toLines :: BSL.ByteString -> [Text]
-    toLines = undefined -- TODO
-    removeComment = undefined -- TODO
-    isWhiteLine = undefined -- TODO
+    toLines = map TL.toStrict . TL.lines . TL.decodeUtf8
+    removeComment = T.takeWhile (\c -> c /= '#')
+    isWhiteLine = T.all isSpace
     f acc line = do
       (c, vers) <- parsePkgVersionsLine line
       return $ addVersions c vers acc
